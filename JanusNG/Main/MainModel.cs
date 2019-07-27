679b7eefc99550f5580710766b7e72e934977334
Andrew Koryavchenko <avk@rsdn.ru>
2019-07-27 19:24:10 +0300

    Messages demo

---
+++ JanusNG/Main/MainModel.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using Rsdn.Api.Models.Accounts;
+using Rsdn.Api.Models.Forums;
+using Rsdn.Api.Models.Messages;
+
+namespace Rsdn.JanusNG.Main
+{
+	public class MainModel : INotifyPropertyChanged
+	{
+		private ForumDescription[] _forums;
+		private MessageInfo[] _topics;
+		private MessageInfo _message;
+		private bool _isSignedIn;
+		private AccountInfo _me;
+
+		public bool IsSignedIn
+		{
+			get => _isSignedIn;
+			set
+			{
+				_isSignedIn = value;
+				OnPropertyChanged(nameof(IsSignedIn));
+				OnPropertyChanged(nameof(IsNotSignedIn));
+			}
+		}
+
+		public bool IsNotSignedIn => !IsSignedIn;
+
+		public AccountInfo Me
+		{
+			get => _me;
+			set
+			{
+				_me = value;
+				OnPropertyChanged(nameof(Me));
+			}
+		}
+
+		public ForumDescription[] Forums
+		{
+			[UsedImplicitly] get => _forums;
+			set
+			{
+				_forums = value;
+				OnPropertyChanged(nameof(Forums));
+			}
+		}
+
+		public MessageInfo[] Topics
+		{
+			get => _topics;
+			set
+			{
+				_topics = value;
+				OnPropertyChanged(nameof(Topics));
+			}
+		}
+
+		public MessageInfo Message
+		{
+			get => _message;
+			set
+			{
+				_message = value;
+				OnPropertyChanged(nameof(Message));
+			}
+		}
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ JanusNG/Main/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using JanusNG;
+using Rsdn.Api.Models.Forums;
+using Rsdn.Api.Models.Messages;
+using Rsdn.ApiClient;
+
+namespace Rsdn.JanusNG.Main
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		private static readonly Uri _rsdnUri = new Uri("https://api.rsdn.org");
+
+		private RsdnApiClient _rsdnClient;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			_rsdnClient = RsdnClientHelpers.CreateAnonymousClient(_rsdnUri);
+		}
+
+		private async void SignInClick(object sender, RoutedEventArgs e)
+		{
+			var tokenFactory = await RsdnSignIn.SignInAsync();
+			_rsdnClient = RsdnClientHelpers.CreateClient(_rsdnUri, tokenFactory);
+			Model.Me = await _rsdnClient.Accounts.GetMeAsync();
+
+			// Bring to front
+			Activate();
+			Topmost = true;  // important
+			Topmost = false; // important
+			Focus();
+
+			Model.IsSignedIn = true;
+			await ReloadModel();
+		}
+
+		private async void WindowLoaded(object sender, RoutedEventArgs e)
+		{
+			await ReloadModel();
+		}
+
+		private async Task ReloadModel()
+		{
+			Model.Forums = await _rsdnClient.Forums.GetForumsAsync();
+		}
+
+		private async void ForumsSelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var forum = (ForumDescription)ForumsList.SelectedItem;
+			Model.Topics =
+				forum != null
+				? (await _rsdnClient.Messages.GetMessagesAsync(
+					limit: 100,
+					forumID: forum.ID,
+					onlyTopics: true))
+					.Items
+				: null;
+		}
+
+		private async void MessagesSelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var msg = (MessageInfo) MessagesList.SelectedItem;
+			Model.Message =
+				msg != null
+				? await _rsdnClient
+					.Messages
+					.GetMessageAsync(msg.ID, withRates: true, withBodies: true, formatBody: true)
+				: null;
+			MessageBrowser.NavigateToString(
+				"<head><meta http-equiv='Content-Type' content='text/html;charset=UTF-8'></head>"
+				+ Model.Message?.Body?.Text ?? " ");
+		}
+	}
+}
