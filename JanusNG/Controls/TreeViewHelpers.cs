f23b791512cc80534892540da75a4f9167a94dfc
Andrew Koryavchenko <avk@rsdn.ru>
2019-08-05 00:17:45 +0300

    MVVM related staff

---
+++ JanusNG/Controls/TreeViewHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using CodeJam;
+
+namespace Rsdn.JanusNG.Controls
+{
+	public static class TreeViewHelpers
+	{
+		public static void SelectItem(this TreeView treeView, params object[] itemPath)
+		{
+			Code.NotNull(treeView, nameof(treeView));
+			Code.NotNull(itemPath, nameof(itemPath));
+
+			ItemsControl container = treeView;
+			TreeViewItem tvi = null;
+			foreach (var node in itemPath)
+			{
+				if (container.ItemContainerGenerator.Status != GeneratorStatus.ContainersGenerated)
+					container.UpdateLayout();
+				tvi = (TreeViewItem) container.ItemContainerGenerator.ContainerFromItem(node);
+				if (tvi == null)
+					return;
+				tvi.IsExpanded = true;
+				container = tvi;
+			}
+
+			if (tvi != null)
+			{
+				tvi.IsSelected = true;
+				tvi.BringIntoView();
+			}
+		}
+	}
+}
